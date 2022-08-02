1e09e7bdd516e7734a7cbcf3a7c36325515699b7
HenryPeeples <hgpeeples48@gmail.com>
2022-08-02 14:19:55 -0500

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p className="text-3xl font-bold underline">Hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
