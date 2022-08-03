7c3a9265b4ed6e434efe0550bbc90e84c07e611f
HenryPeeples <hgpeeples48@gmail.com>
2022-08-02 21:44:34 -0500

    [Socials]: Add social icons and links

---
+++ src/components/Socials.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { GithubFill, TwitterFill, InstagramFill } from "akar-icons";
+
+const Socials = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col lg:w-1/3 md:w-1/3 w-1/2 h-fit justify-between items-center pt-6">
+        <p>Check out my socials!</p>
+        <div className="flex flex-row gap-x-11 pt-3">
+          <div className="p-1 hover:text-primary hover:shadow-social rounded transition-all ease-in-out duration-300">
+            <a
+              href="https://github.com/hankpeeples"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubFill strokeWidth={2} size={30} />
+            </a>
+          </div>
+          <div className="p-1 hover:text-[#1DA1F2] hover:shadow-social hover:shadow-[#1DA1F2] rounded transition-all ease-in-out duration-300">
+            <a
+              href="https://twitter.com/HankPeeples"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TwitterFill strokeWidth={2} size={30} />
+            </a>
+          </div>
+          <div className="p-1 hover:text-[#F56040] hover:shadow-social hover:shadow-[#F56040]  rounded transition-all ease-in-out duration-300">
+            <a
+              href="https://www.instagram.com/hankpeeples1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <InstagramFill strokeWidth={2} size={30} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Socials;
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
       },
       boxShadow: {
         btn: "2px 2px 6px",
+        social: "0px 0px 10px",
       },
     },
     fontFamily: {
