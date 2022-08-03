b36efd6493ee82c83ae4ae4146bb3540ebc62dc6
HenryPeeples <hgpeeples48@gmail.com>
2022-08-03 16:12:14 -0500

    [NavBar]: Fix small screen layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import Button from "./Button";
 const NavBar = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="flex flex-row lg:w-1/2 md:w-2/3 w-screen h-fit justify-between items-center p-3">
+      <div className="flex flex-col md:flex-row lg:w-1/2 md:w-2/3 w-screen h-fit justify-between items-center p-3">
         <img width="200em" src={require("../assets/logo.png")} alt="Personal logo" />
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-row pt-3 gap-8">
           <Button color="primary">Projects</Button>
           <Button color="secondary">Posts</Button>
         </div>
