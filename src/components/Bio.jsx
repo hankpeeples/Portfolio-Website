f44bafc64e8b8177b64ae2d69a83ac31521dc493
HenryPeeples <hgpeeples48@gmail.com>
2022-08-06 01:37:14 -0500

    [Bio]: Adjust responsive layout

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,8 +4,10 @@ import BioYear from "./BioYear";
 const Bio = () => {
   return (
     <div className="flex w-screen justify-center items-center">
-      <div className="flex flex-col w-1/2 p-5 pt-10">
-        <h2 className="text-xl font-proximaBold underline justify-start">Bio</h2>
+      <div className="flex flex-col md:w-1/2 p-5 pt-10">
+        <h2 className="text-lg md:text-xl font-proximaBold underline justify-start">
+          Bio
+        </h2>
         <BioYear year="1999">Born in Plano, Texas</BioYear>
         <BioYear year="2023">
           Graduated from The University of North Texas with a bachelors degree in
--- src/components/BioYear.jsx
+++ src/components/BioYear.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const BioYear = (props) => {
   return (
     <div className="flex pt-2">
-      <div className="text-lg flex ">
-        <span className="font-proximaBold pr-5">{props.year}</span>
+      <div className="text-md md:text-lg flex">
+        <span className="font-proximaBold mr-5">{props.year}</span>
         {props.children}
       </div>
     </div>
