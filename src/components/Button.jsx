98430c40118f16cf22ab17a56d490afa1f23a551
HenryPeeples <hgpeeples48@gmail.com>
2022-08-06 00:17:38 -0500

    [Button]: Increase button comp. re-usability

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 
 const Button = (props) => {
-  if (props.color === "primary") {
-    return (
-      <div className="">
-        <button className="bg-primary text-black py-1 px-3 rounded hover:shadow-btn hover:shadow-primary transition-shadow ease-in-out duration-300">
-          {props.children}
-        </button>
-      </div>
-    );
-  } else if (props.color === "secondary") {
-    return (
-      <div className="">
-        <button className="bg-secondary text-black py-1 px-3 rounded hover:shadow-btn hover:shadow-secondary transition-shadow ease-in-out duration-300">
-          {props.children}
-        </button>
-      </div>
-    );
-  } else if (props.color === "accent") {
-    return (
-      <div className="">
-        <button className="bg-accent text-black py-1 px-3 rounded hover:shadow-btn hover:shadow-accent transition-shadow ease-in-out duration-300">
-          {props.children}
-        </button>
-      </div>
-    );
-  }
+  return (
+    <button
+      className={`${props.color} py-1 px-3 rounded hover:shadow-btn transition-shadow ease-in-out duration-300`}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
