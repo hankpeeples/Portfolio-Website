f7e95f36f9af41b9825bae20b5eb4aa807106978
HenryPeeples <hgpeeples48@gmail.com>
2022-08-13 13:04:22 -0500

    [Exp]: Update icons and tooltips

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,12 @@ import Go from "../Icons/go.svg";
 import Java from "../Icons/java.svg";
 import JSIcon from "../Icons/javascript.svg";
 import TSIcon from "../Icons/typescript.svg";
+import ReactIcon from "../Icons/react.svg";
+import VSCode from "../Icons/vscode.svg";
+import DockerIcon from "../Icons/docker.svg";
+import IntellijIcon from "../Icons/intellij.svg";
+import Trello from "../Icons/trello.svg";
+import ReactTooltip from "react-tooltip";
 
 const Experience = () => {
   return (
@@ -18,20 +24,93 @@ const Experience = () => {
         <h2 className="flex text-lg md:text-xl font-proximaBold underline justify-start">
           Languages, Frameworks, Tools
         </h2>
-        <div className="flex flex-row gap-6 pt-2">
-          <img src={Go} alt="Go Icon" height={46} width={46} />
-          <img src={Java} alt="Java Icon" height={46} width={46} />
-          <img src={Cpp} alt="Cpp Icon" height={46} width={46} />
-          <img src={CIcon} alt="C Icon" height={46} width={46} />
-          <img src={JSIcon} alt="JSIcon Icon" height={46} width={46} />
-          <img src={TSIcon} alt="TSIcon Icon" height={46} width={46} />
+        <ReactTooltip
+          place="top"
+          type="info"
+          effect="solid"
+          className="!font-proximaBold !text-[16px]"
+        />
+        <div className="flex flex-row gap-8 pt-3">
+          <img data-tip="Golang" src={Go} alt="Go Icon" height={46} width={46} />
+          <img data-tip="Java" src={Java} alt="Java Icon" height={46} width={46} />
+          <img data-tip="C++" src={Cpp} alt="Cpp Icon" height={46} width={46} />
+          <img data-tip="C" src={CIcon} alt="C Icon" height={46} width={46} />
+          <img
+            data-tip="JavaScript"
+            src={JSIcon}
+            alt="JSIcon Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="TypeScript"
+            src={TSIcon}
+            alt="TSIcon Icon"
+            height={46}
+            width={46}
+          />
         </div>
-        <div className="flex flex-row gap-6 pt-3">
-          <img src={GithubLogo} alt="Github Icon" height={46} width={46} />
-          <img src={Git} alt="Git Icon" height={46} width={46} />
-          <img src={SSHIcon} alt="SSH Icon" height={46} width={46} />
-          <img src={Linux} alt="Linux Icon" height={46} width={46} />
-          <img src={MySql} alt="MySql Icon" height={46} width={46} />
+        <div className="flex flex-row gap-8 pt-4">
+          <img
+            data-tip="Github"
+            src={GithubLogo}
+            alt="Github Icon"
+            height={46}
+            width={46}
+          />
+          <img data-tip="Git" src={Git} alt="Git Icon" height={46} width={46} />
+          <img data-tip="SSH" src={SSHIcon} alt="SSH Icon" height={46} width={46} />
+          <img
+            data-tip="MySql"
+            src={MySql}
+            alt="MySql Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="React"
+            src={ReactIcon}
+            alt="React Icon"
+            height={46}
+            width={46}
+          />
+        </div>
+        <div className="flex flex-row gap-8 pt-4">
+          <img
+            data-tip="Docker"
+            src={DockerIcon}
+            alt="Docker Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="Trello"
+            src={Trello}
+            alt="Trello Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="Intellij IDEs"
+            src={IntellijIcon}
+            alt="Intellij Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="VSCode"
+            src={VSCode}
+            alt="VSCode Icon"
+            height={46}
+            width={46}
+          />
+          <img
+            data-tip="Linux"
+            src={Linux}
+            alt="Linux Icon"
+            height={46}
+            width={46}
+          />
         </div>
       </div>
     </div>
