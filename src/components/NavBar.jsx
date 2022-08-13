4489ef0fc2ba9f81d39c2d583c0f2a68fc85df5e
HenryPeeples <hgpeeples48@gmail.com>
2022-08-13 13:22:01 -0500

    [NavBar]: Make navbar sticky

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center sticky top-0 bg-[#2f2f2fea]">
       <div className="flex flex-col md:flex-row lg:w-1/2 md:w-2/3 w-screen h-fit justify-between items-center p-3">
         <img width="200em" src={require("../assets/logo.png")} alt="Personal logo" />
         <div className="flex flex-row pt-5 md:pt-0 gap-8">
