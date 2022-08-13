ab6747af76af72e591dfb1192e6e19c2a83de620
HenryPeeples <hgpeeples48@gmail.com>
2022-08-13 13:40:10 -0500

    [Footer]: Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  let date = new Date();
+
+  return (
+    <div className="flex w-screen justify-center items-center">
+      <div className="flex w-screen md:w-2/3 lg:w-1/2 h-fit p-5 pt-10 justify-center items-center gap-8">
+        <p className="text-sm text-[#8c8c8c]">
+          © {date.getFullYear()} Henry Peeples
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
