97e922a3b3b06769866e5c2b2a0d4aa34110bcd9
HenryPeeples <hgpeeples48@gmail.com>
2022-08-14 12:09:16 -0500

    [App]: Add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="font-proxima text-white bg-[#2f2f2f] h-full w-screen">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
