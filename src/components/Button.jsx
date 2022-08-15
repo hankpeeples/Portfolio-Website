4b6441d900b48fab8b4cc809202064996d70d5b0
HenryPeeples <hgpeeples48@gmail.com>
2022-08-15 18:02:27 -0500

    [Button]: Update prop name

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
 
   return (
     <button
-      className={`${props.color} py-1 px-3 rounded hover:shadow-btn transition-shadow ease-in-out duration-300`}
+      className={`${props.colors} py-1 px-3 rounded hover:shadow-btn transition-shadow ease-in-out duration-300`}
       onClick={() => navigate(props.link)}
     >
       {props.children}
