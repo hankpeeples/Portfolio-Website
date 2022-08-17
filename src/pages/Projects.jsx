bc87f36da4c07355bbfadeb3693d4174da9c5db3
HenryPeeples <hgpeeples48@gmail.com>
2022-08-17 18:40:24 -0500

    [Projects]: Add github api fetch

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ProfileCard from "../components/Projects/ProfileCard";
+import Loading from "../components/Loading";
+import RepoCard from "../components/Projects/RepoCard";
 
 const Projects = () => {
+  const [loading, setLoading] = useState(true);
+  const [githubRepos, setGithubRepos] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://api.github.com/users/hankpeeples/repos")
+      .then((res) => {
+        setGithubRepos(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log("Failed to fetch projects: " + err))
+      .then(() => setLoading(true));
+  }, []);
+
   return (
     <div className="flex w-screen h-screen justify-center">
       <div className="flex flex-col w-screen md:w-2/3 lg:w-1/2 h-fit p-3 mt-10 gap-10">
         <h1 className="text-3xl font-proximaBold underline">My Projects</h1>
-        <ProfileCard />
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <ProfileCard />
+            <RepoCard />
+          </>
+        )}
       </div>
     </div>
   );
