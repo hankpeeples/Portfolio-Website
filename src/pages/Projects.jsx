8956b3a9aa7c52ca1852498c58ae61cc19331204
HenryPeeples <hgpeeples48@gmail.com>
2022-08-18 13:36:36 -0500

    Remove unnecessary github profile card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ProfileCard from "../components/Projects/ProfileCard";
 import Loading from "../components/Loading";
 import RepoCard from "../components/Projects/RepoCard";
 
@@ -31,16 +30,13 @@ const Projects = () => {
         {loading || githubRepos[0] === undefined ? (
           <Loading />
         ) : (
-          <>
-            <ProfileCard />
-            <div className="flex flex-row flex-wrap gap-10 justify-between">
-              {githubRepos.map((value, index) => {
-                if (value.name !== "hankpeeples")
-                  return <RepoCard data={value} key={index} />;
-                else return null;
-              })}
-            </div>
-          </>
+          <div className="flex flex-row flex-wrap gap-10 justify-between">
+            {githubRepos.map((value, index) => {
+              if (value.name !== "hankpeeples")
+                return <RepoCard data={value} key={index} />;
+              else return null;
+            })}
+          </div>
         )}
       </div>
     </div>
