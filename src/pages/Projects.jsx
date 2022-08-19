9966d6f3350447959c344bd78cc8fdbbb848d7a1
HenryPeeples <hgpeeples48@gmail.com>
2022-08-19 11:39:59 -0500

    [Projects]: Fix scroll catching bug

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -43,7 +43,7 @@ const Projects = () => {
   }
 
   return (
-    <div className="flex w-screen h-screen justify-center overflow-scroll">
+    <div className="flex w-screen h-fit justify-center">
       <div className="flex flex-col w-screen md:w-2/3 lg:w-1/2 h-fit p-3 mt-10 gap-10">
         <h1 className="text-3xl font-proximaBold underline">My Projects</h1>
         {loading || githubRepos === undefined ? (
